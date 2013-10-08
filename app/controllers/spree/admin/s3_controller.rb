7e593fe4ad5bffe76196fbc488ecb35365e5525a
Scott Tesoriere <scott@tesoriere.com>
2013-10-08 09:45:20 -0400

    reverted

--- app/controllers/spree/admin/s3_controller.rb
+++ app/controllers/spree/admin/s3_controller.rb
@@ -1,7 +1,7 @@
 class Spree::Admin::S3Controller < Spree::Admin::BaseController
 
 	def index
-    response = { filelink: "https://#{Spree::Config.get_preference(:s3_bucket)}.s3.amazonaws.com/#{params[:key]}" }
+    response = { filelink: "http://#{Spree::Config.get_preference(:s3_bucket)}.s3.amazonaws.com/#{params[:key]}" }
     render json: response, status: :ok
 	end
 end
